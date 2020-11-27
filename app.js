73cdfec5b16a4f35b9cbdbc6a088770ea1eeb71c
Tim Moonen <info@moonendesign.nl>
2020-11-27 15:28:46 +0100

    Changed styling of hex code

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 function changeColor() {
     var randomColor = Math.floor(Math.random()*16777215).toString(16);
         document.body.style.backgroundColor = '#' + randomColor;
+        document.getElementById("color").style.color = '#' + randomColor;
         color.innerHTML = "#" + randomColor;
         return true;
     }
